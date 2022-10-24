75f3fbe0eb01840936b1705fb667b999fb86a616
poonghyeok <vpd3302@naver.com>
2022-10-24 21:08:17 +0900

    221024todolist

---
+++ vue-todo/todolist/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- 어플리케이션 이름을 표시한다.  -->
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+/* scoped는 vue에서 지원하는 속성, 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미 */
+    h1{
+        color : #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
